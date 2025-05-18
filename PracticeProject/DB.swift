01860026f5e0177d695a49edbbf90d4dd25d0c57
MaximB0nd <sersim11@yandex.ru>
2025-05-18 22:02:40 +0300

    Using DB SQLite

---
+++ PracticeProject/DB.swift
@@ -0,0 +1,6 @@
+//
+//  DB.swift
+//  PracticeProject
+//
+//  Created by Максим Бондарев on 17.05.2025.
+//
--- PracticeProject/main.swift
+++ PracticeProject/main.swift
@@ -6,9 +6,55 @@
 //
 
 import Foundation
+import SQLite
 
-let a: [Int] = RandomArray(count: 10, range: 1..<100)
-let b: [Int] = RandomArray(count: 10, range: 1..<100)
+do {
+    
+    _ = copyDatabaseIfNeeded(sourcePath: "identifier.sqlite")
 
-print(SwapOdds(array_1: a, array_2: b))
+    let db = try Connection("identifier.sqlite")
+    
+    let users = Table("Users")
+    
+    let userId = Expression<Int64>("id")
+    let userName = Expression<String>("name")
+    let userEmail = Expression<String>("email")
+    let password = Expression<String>("password")
+    
+    // Creating table in db
+    try db.run(users.create { t in
+        t.column(userId, primaryKey: true)
+        t.column(userName)
+        t.column(userEmail)
+        t.column(password)
+    })
+
+    // Insert data in db
+    try db.run(users.insert(userId <- 2, userName <- "Maxim", userEmail <- "max@mail.ru", password <- "123"))
+    
+    // Selecting values from table in db
+    for user in try db.prepare(users) {
+        print(try user.get(userName))
+    }
+}
+
+catch {
+    print(error)
+}
+
+
+
+func copyDatabaseIfNeeded(sourcePath: String) -> Bool {
+    let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+    let destinationPath = documents + "/identifier.sqlite"
+    let exists = FileManager.default.fileExists(atPath: destinationPath)
+    guard !exists else { return false }
+    do {
+        try FileManager.default.copyItem(atPath: sourcePath, toPath: destinationPath)
+        return true
+    } catch {
+      print("error during file copy: \(error)")
+        return false
+    }
+}
 
